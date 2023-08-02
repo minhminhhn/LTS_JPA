e5dc0e024b6882d1ffed97c128ca95a36de1b3b2
minhminhhn <anilinform23@gmail.com>
2023-08-02 13:55:03 +0700

    02/08

---
+++ quanlytrungtam/src/main/java/com/example/quanlytrungtam/QuanlytrungtamApplication.java
@@ -0,0 +1,13 @@
+package com.example.quanlytrungtam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlytrungtamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlytrungtamApplication.class, args);
+	}
+
+}
---
+++ quanlytrungtam/src/test/java/com/example/quanlytrungtam/QuanlytrungtamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quanlytrungtam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlytrungtamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
