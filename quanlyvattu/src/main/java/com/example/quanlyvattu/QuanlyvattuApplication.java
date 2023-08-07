3cf13f618d4f756aca372cf80eac1ea165c09a87
minhminhhn <anilinform23@gmail.com>
2023-08-07 10:24:21 +0700

    07/08

---
+++ quanlyvattu/src/main/java/com/example/quanlyvattu/QuanlyvattuApplication.java
@@ -0,0 +1,13 @@
+package com.example.quanlyvattu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlyvattuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlyvattuApplication.class, args);
+	}
+
+}
---
+++ quanlyvattu/src/test/java/com/example/quanlyvattu/QuanlyvattuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quanlyvattu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlyvattuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
